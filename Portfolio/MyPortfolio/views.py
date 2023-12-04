3b9cf83bff5e90cf6df18085e8984212183e37f6
Oh-YoungHwan <141305318+Oh-YoungHwan@users.noreply.github.com>
2023-12-04 15:28:13 +0900

    - 장고 프로젝트 test.html 저장
    - 장고 프로젝트 test.html 저장 : 231204

--- Portfolio/MyPortfolio/views.py
+++ Portfolio/MyPortfolio/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import admin_post
 
 # Create your views here.
+
+def test(request) :
+    tests = admin_post
+
+    return render(request, "templates/test.html", {"tests":tests})
