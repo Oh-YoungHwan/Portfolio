66e472a639357a128561ef5740778b3970234d18
Oh-YoungHwan <141305318+Oh-YoungHwan@users.noreply.github.com>
2024-01-10 13:13:33 +0900

    - 배포 준비
    - Debug 값을 False로 수정

--- Portfolio/Portfolio/settings.py
+++ Portfolio/Portfolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-i3cafmjs)q6ty=2oeiq-8v3e6*m6aov+%wb6_ec8bhfy-buy2#"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
